d91bd52d994733197bc008258abd27a6e505f4be
Patrick Pelletier <code@funwithsoftware.org>
2018-09-12 16:13:59 -0700

    Set up dotstar_wing namespace.

--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+/**
+ * DotStar FeatherWing used with feather:bit.
+ */
+//% color="#ffff00" weight=50 icon="\uf0eb" block="DotStar Wing"
+namespace dotstar_wing {
+}
